d98f01d9f688934d7963c3ccb5062d6f1419a1e0
Eldus <arsik2001@gmail.com>
2021-01-21 12:36:28 +0200

    Task zero

--- src/com/eldus/Main.java
+++ src/com/eldus/Main.java
@@ -4,16 +4,20 @@ import java.util.*;
 
 public class Main {
 
-    public static <N> Number sumReturn(List<Number> numbers) {
-        return 0;
+    public static <N extends Number> Double sumReturn(List<N> numbers) {
+        double sum = 0;
+        for(N number : numbers){
+            sum += number.doubleValue();
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-//        List<Number> list = new ArrayList<>();
-//        list.add(1.5);
-//        list.add(2.7);
-//        list.add(3.3);
-//        System.out.println(sumReturn(list));
+        List<Number> list2 = new ArrayList<>();
+        list2.add(1.5);
+        list2.add(2.7);
+        list2.add(3.3);
+        System.out.println(sumReturn(list2));
         Employee e1 = new Employee(10, 2500, "Марк");
         Employee e2 = new Employee(2, 5200, "Александр");
         Employee e3 = new Employee(100, 2500, "Алексей");
@@ -29,7 +33,7 @@ public class Main {
 
         Collections.sort(list);
         System.out.println(list);
-        Comparator comparator = new EmployeeBySalaryComparator();
+        Comparator<Employee> comparator = new EmployeeBySalaryComparator();
         Collections.sort(list, comparator);
         System.out.println(list);
 
